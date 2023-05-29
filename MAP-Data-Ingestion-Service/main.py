bd7645666539ab27fb6a5e7993798e5e16dbdabe
Lasse Birk <lassebirk.birk9@gmail.com>
2023-05-29 15:01:08 +0200

    fresg

--- MAP-Data-Ingestion-Service/main.py
+++ MAP-Data-Ingestion-Service/main.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from LogicLayer import ingestion_job_handler
-#test
+#testsef
 app = Flask(__name__)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5001))
